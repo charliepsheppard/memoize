d50333f92c164339630f2572efa78bdccc095ef6
Charlie Sheppard <sheppard.charlie24@gmail.com>
2023-07-25 09:05:09 -0700

    add login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import Register from './Register';
 
 function App() {
 
   return (
-    <div className="bg-red-500">Test</div>
+    <Register />
   )
 }
 
