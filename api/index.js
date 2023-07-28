21259914a31895c0e23455e1fab907441279d653
Charlie Sheppard <sheppard.charlie24@gmail.com>
2023-07-28 15:43:36 -0700

    add login functionality

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
@@ -11,6 +12,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   credentials: true,
@@ -21,14 +23,27 @@ app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
+app.get('/profile', (req, res) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json('no token, sorry');
+  }
+});
+
 app.post('/register', async (req, res) => {
   const {username, password} = req.body;
   try {
     const createdUser = await User.create({ username, password });
     jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token', token).status(201).json({
-        _id: createdUser._id,
+      res.cookie('token', token, {sameSite: 'none', secure: true}).status(201).json({
+        id: createdUser._id,
+        username
       });
     });
   } catch(err) {
